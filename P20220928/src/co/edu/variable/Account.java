824a38cb101239d04e1a0a99b6417f4af679b3d0
Kim-nemo <rhakdnj1232@naver.com>
2022-09-30 17:40:47 +0900

    0930

--- P20220928/src/co/edu/variable/Account.java
+++ P20220928/src/co/edu/variable/Account.java
@@ -1,7 +1,47 @@
 package co.edu.variable;
 
-public class Account {
-	String accNo; // 계좌번호.  두 단어의 조합: 2번째 첫번째는 대문자로. 
-	String owner; // 예금주
-	int balance; // 예금액
+public class Account {	
+	private String accNo; // 계좌번호.  두 단어의 조합: 2번째 첫번째는 대문자로. 
+	private String owner; // 예금주
+	private int balance; // 예금액
+
+	void setAccNo(String accNo) {
+		this.accNo = accNo;
+	}
+	void setOwner(String owner) {
+		this.owner = owner;
+	}
+	void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+	String getAccNo() {
+		return this.accNo;
+	}
+	String getOwner() {
+		return this.owner;
+	}
+	int getBalance() {
+		return this.balance;
+	}
+	
+		
+//	public static void main(String[] args) {
+//		WhileBankApp b1 = new WhileBankApp();
+//		
+//		b1.setAccNo("1-1111");
+//		b1.setOwner("홍길동");
+//		b1.setBalance(10000);
+//		
+//		System.out.println(b1.getAccNo);
+//	}
+	/*
+	private String accNo; // 계좌번호.  두 단어의 조합: 2번째 첫번째는 대문자로. 
+	private String owner; // 예금주
+	private int balance; // 예금액
+	*/
+	
+	// setter, getter 정의
+	
+	
 }
--- P20220928/src/co/edu/variable/WhileBankApp.java
+++ P20220928/src/co/edu/variable/WhileBankApp.java
@@ -9,8 +9,8 @@ public class WhileBankApp {
 	public static void main(String[] args) {
 		// 메뉴: 1.예금, 2:출금, 3:잔액, 4:종료
 		Account[] banks = new Account[10];
-		
 		Scanner scn = new Scanner(System.in);
+		
 		while(true) {
 			System.out.println("0.계좌생성 1.예금, 2:출금, 3:잔액, 4:종료");
 			int menu = scn.nextInt(); scn.nextLine();
@@ -19,17 +19,20 @@ public class WhileBankApp {
 				// 예금기능. 계좌번호, 얼마?
 				System.out.println("계좌번호>>> ");
 				String accNo = scn.nextLine();
+				
 				System.out.println("예금할 금액>>> ");
 				int money = scn.nextInt();
 				
+				boolean check = true;
 				for(int i=0; i<banks.length; i++) {
-					if(banks[i] != null && banks[i].accNo.equals(accNo)) {
+					if(banks[i] != null && banks[i].getAccNo().equals(accNo)) {
 					// null인지 아닌지 체크하는 과정
-						banks[i].balance = banks[i].balance + money;
-					}else {
-						System.out.println("입력하신 계좌가 존재하지 않습니다");
+						banks[i].setBalance(banks[i].getBalance() + money);
+						check = false;
 						break;
 					}
+				}if(check) {
+					System.out.println("입력하신 계좌가 존재하지 않습니다");
 				}
 			}else if(menu == 2) {
 				System.out.println("계좌번호>>> ");
@@ -37,26 +40,31 @@ public class WhileBankApp {
 				System.out.println("출금할 금액>>> ");
 				int money = scn.nextInt();
 				
+				boolean check = true;
 				for(int i=0; i<banks.length;i++) {
-					if(banks[i] != null && banks[i].accNo.equals(accNo)) {												
-						if(banks[i].balance-money>0) {
-							banks[i].balance = banks[i].balance - money;							
-						}else {
-							System.out.println("잔고부족");
+					if(banks[i] != null && banks[i].getAccNo().equals(accNo)) {												
+						if(banks[i].getBalance()-money>0) {
+							banks[i].setBalance(banks[i].getBalance() - money);
+							check = false;
 							break;
 						}
 					} 
-				}				
+				}if(check) {
+					System.out.println("잔고부족");
+				}
 			}else if(menu == 3) {
 				System.out.println("조회할 계좌번호 입력>>> ");
 				String accNo = scn.nextLine();
+				
+				boolean check = true;
 				for(int i = 0; i <banks.length; i++) {
-					if(banks[i] != null && banks[i].accNo.equals(accNo)) {
-						System.out.println("잔액: "+banks[i].balance);
-					}else {
-						System.out.println("입력하신 계좌번호가 존재하지 않습니다");
+					if(banks[i] != null && banks[i].getAccNo().equals(accNo)) {
+						System.out.println("잔액: "+banks[i].getBalance());
+						check = false;
 						break;
 					}
+				}if(check) {
+					System.out.println("입력하신 계좌번호가 존재하지 않습니다");
 				}
 				
 			}else if(menu == 4) {
@@ -67,10 +75,12 @@ public class WhileBankApp {
 				String accNo = scn.nextLine();
 				System.out.println("예금주 이름 입력>>> ");
 				String owner = scn.nextLine();
+				
+				
 
 				Account acc = new Account(); // 인스턴스 생성
-				acc.accNo = accNo;
-				acc.owner = owner;
+				acc.setAccNo(accNo);
+				acc.setOwner(owner);
 				
 				// 계좌번호 생성
 				for(int i=0; i<banks.length; i++) {
