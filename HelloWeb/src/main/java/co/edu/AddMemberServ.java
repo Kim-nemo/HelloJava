f38d7eb53878d7cd64ec3689accadd8799f77079
Kim-nemo <rhakdnj1232@naver.com>
2022-10-21 17:45:36 +0900

    1021

---
+++ HelloWeb/src/main/java/co/edu/AddMemberServ.java
@@ -0,0 +1,74 @@
+package co.edu;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import co.edu.emp.EmpDAO;
+import co.edu.emp.EmployeeVO;
+
+/**
+ * Servlet implementation class AddMemberServ
+ */
+@WebServlet({ "/addMemberServlet", "/addMember" })
+public class AddMemberServ extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddMemberServ() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		// content 타입을 지정
+		response.setContentType("text/html;charset=UTF-8"); // 보여지는 페이지의 타입을 html로 설정
+		
+		System.out.println("/addMemberSer 페이지입니다.");
+		String empId = request.getParameter("employee_id");
+		String lName = request.getParameter("last_name");
+		String email = request.getParameter("email");
+		String hireDate = request.getParameter("hire_date");
+		String jobId = request.getParameter("job_id");
+		
+		System.out.println(empId);
+		
+		// 입력
+		EmpDAO dao = new EmpDAO();
+		EmployeeVO vo = new EmployeeVO(0, lName, email, hireDate, jobId);
+		dao.insertEmp(vo);
+		
+		System.out.println("입력완료");
+		
+		PrintWriter out = response.getWriter(); // 출력 스트림
+		out.print("<h3>completed.</h3>");
+		out.print("<p>사원번호 : " +vo.getEmployeeId()+ "</p>");
+		out.print("<p>성씨 : " + vo.getLastName()+ "</p>");
+		out.print("<p>이메일 : " +vo.getEmail()+ "</p>");
+		out.print("<p>입사일시 : " +hireDate + "</p>");
+		out.print("<p>직부 : " +jobId + "</p>");
+		
+		out.print("<a href = index.html>첫 페이지로 이동</a>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ HelloWeb/src/main/java/co/edu/emp/EmpDAO.java
@@ -0,0 +1,124 @@
+package co.edu.emp;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmpDAO extends DAO { // 데이터 베이스 처리하는 곳
+	
+	// 입력
+	public EmployeeVO insertEmp(EmployeeVO vo) { // 매개값으로 받았던 vo에 시퀀스 값을 추가해주기위해 void대신 EmployeeVO사용
+		conn = getConnect();
+		
+		String seq = "select employees_seq.nextval from dual";
+		
+		String sql = "insert into empl (employee_id, first_name, last_name, email, hire_date, job_id) "
+					+ "values(?, ?, ?, ?, ?, ?)";
+		
+		try {
+			// 시퀀스 획득
+			int seqInt = 0;
+			psmt = conn.prepareStatement(seq);
+			rs = psmt.executeQuery();
+			if(rs.next()) {
+				seqInt = rs.getInt(1); // 첫번째 칼럼을 가져오겠습니다
+			}
+			//insert 작업
+			psmt = conn.prepareStatement(sql);
+			psmt.setInt(1, seqInt);
+			psmt.setString(2, vo.getFirstName());
+			psmt.setString(3, vo.getLastName());
+			psmt.setString(4, vo.getEmail());
+			psmt.setString(5, vo.getHireDate());
+			psmt.setString(6, vo.getJobId());
+			int r = psmt.executeUpdate(); // 쿼리실행.
+			System.out.println(r+"건 입력됨");
+			
+			// 새로운 입력하게 사원번호
+			vo.setEmployeeId(seqInt);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+		return vo;
+	}
+	// 삭제
+	public void deleteEmp(int employeeId) {
+		getConnect();
+		String sql = "delete from empl where employee_id = ?"; // employee_id 기준으로 삭제
+		try {
+			psmt = conn.prepareStatement(sql);
+			psmt.setInt(1, employeeId);
+			int r = psmt.executeUpdate();
+			System.out.println(r+"건 삭제됨");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+	}
+	// 수정
+	public void updateEmp(EmployeeVO vo) {
+		getConnect();
+		String sql = "update empl set first_name = ? , last_name =?, email=?, hire_date=? , job_id = ? where employee_id = ?";
+		try {
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, vo.getFirstName());
+			psmt.setString(2, vo.getLastName());
+			psmt.setString(3, vo.getEmail());
+			psmt.setString(4, vo.getHireDate());
+			psmt.setString(5, vo.getJobId());
+			psmt.setInt(6, vo.getEmployeeId());
+			int r = psmt.executeUpdate();
+			System.out.println(r+"건 수정됨");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+	}
+	// 목록
+	public List<EmployeeVO> empList(EmployeeVO vo){
+		List<EmployeeVO> empList = new ArrayList<EmployeeVO>();
+		getConnect();
+		String sql = "select * from empl"
+				+"  where employee_id = decode(?,0, employee_id, ?) "
+				+"  and   first_name like '%'||?||'%' "
+				+"  and   last_name like '%'||?||'%' "
+				+"  and   email like '%'||?||'%' "
+				+"  and   to_char(hire_date,'yyyy-mm-dd') like '%'||?||'%' "
+				+"  and   job_id = nvl(?, job_id) " // ?값이 널이면 job_id자체를 비교
+				+"order by employee_id";
+		
+		try {
+			psmt = conn.prepareStatement(sql);
+			psmt.setInt(1, vo.getEmployeeId());
+			psmt.setInt(2, vo.getEmployeeId());
+			psmt.setString(3, vo.getFirstName());
+			psmt.setString(4, vo.getLastName());
+			psmt.setString(5, vo.getEmail());
+			psmt.setString(6, vo.getHireDate());
+			psmt.setString(7, vo.getJobId());
+			rs = psmt.executeQuery(); //rs<-컬렉션(여러건 담을 수 있음)
+			while(rs.next()) { // 데이터가 있는 건수만큼 담아서 empList에 넣어줌
+				int empId = rs.getInt("employee_id");
+				String fName = rs.getString("first_name");
+				String lName = rs.getString("last_name");
+				String email = rs.getString("email");
+				String hDate = rs.getString("hire_date");
+				String jobId = rs.getString("job_id");
+				
+				EmployeeVO emp = new EmployeeVO(empId, fName, lName, email, hDate, jobId);
+				empList.add(emp);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+		return empList;
+	}	
+}
